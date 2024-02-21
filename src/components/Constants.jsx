607be89e1ec154c48a2218de9f7a868162ba6ff1
Muhammad-Pumex <muhammad@pumexinfotech.com>
2024-02-21 10:11:11 +0530

    Constants page.

---
+++ src/components/Constants.jsx
@@ -0,0 +1,48 @@
+export const images1 = [
+  require("../assets/landing-images/1-houseboat-2031055_1920.jpg"),
+  require("../assets/landing-images/2-mountain-7073990_1920.jpg"),
+  require("../assets/landing-images/3-Rectangle 38.png"),
+  require("../assets/landing-images/4-stones--2040340_1920.jpg"),
+];
+
+export const images2 = [
+  require("../assets/landing-images/4-stones--2040340_1920.jpg"),
+  require("../assets/landing-images/3-Rectangle 38.png"),
+  require("../assets/landing-images/2-mountain-7073990_1920.jpg"),
+  require("../assets/landing-images/1-houseboat-2031055_1920.jpg"),
+];
+
+export const serviceCards = [
+  {
+    image: require("../assets/service-cards/Rectangle 21.png"),
+    name: "Ayurveda Retreat",
+  },
+  {
+    image: require("../assets/service-cards/Rectangle 22.png"),
+    name: "Cultural Retreat",
+  },
+  {
+    image: require("../assets/service-cards/Rectangle 23.png"),
+    name: "Culinary Retreat",
+  },
+  {
+    image: require("../assets/service-cards/Rectangle 24.png"),
+    name: "Modern Beauty Retreat",
+  },
+  {
+    image: require("../assets/service-cards/Rectangle 21.png"),
+    name: "Ayurveda Retreat",
+  },
+  {
+    image: require("../assets/service-cards/Rectangle 22.png"),
+    name: "Cultural Retreat",
+  },
+  {
+    image: require("../assets/service-cards/Rectangle 23.png"),
+    name: "Culinary Retreat",
+  },
+  {
+    image: require("../assets/service-cards/Rectangle 24.png"),
+    name: "Modern Beauty Retreat",
+  },
+];
