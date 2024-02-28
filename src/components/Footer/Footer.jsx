ad4360e6d86eba3f7126bcb395f866f965d97811
Muhammad-Pumex <muhammad@pumexinfotech.com>
2024-02-28 18:12:41 +0530

    Change footer design.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,72 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Buttons from "../Buttons/Buttons";
+import Facebook from "../../assets/footer/facebook-svg.svg";
+import footerLogo from "../../assets/footer/footerLogo.svg";
+import Insta from "../../assets/footer/insta-svg.svg";
+import Linkedin from "../../assets/footer/linkedin-svg.svg";
+import WhatsApp from "../../assets/footer/whatsapp-svg.svg";
+import Xtwitter from "../../assets/footer/x-svg.svg";
 import "./Footer.css";
 
 function Footer() {
   return (
-    <div>
-      <footer>
-        {/* Join Our Newsletter */}
-        <div className="join-our-newsletter">
-          <h3>JOIN OUR NEWSLETTER</h3>
-          <div className="input-button-combo">
-            <input
-              className="email-address-input"
-              placeholder="Email Address"
-            ></input>
-            <Buttons size="medium" text="SUBSCRIBE" />
-          </div>
+    <footer>
+      <div className="footer-wrapper">
+        <div className="social-media-images">
+          <Link to="/about">
+            <img src={WhatsApp} alt="WhatsApp svg"></img>
+          </Link>
+          <Link to="/about">
+            <img src={Xtwitter} alt="Xtwitter svg"></img>
+          </Link>
+          <Link to="/about">
+            <img src={Insta} alt="Insta svg"></img>
+          </Link>
+          <Link to="/about">
+            <img src={Facebook} alt="Facebook svg"></img>
+          </Link>
+          <Link to="/about">
+            <img src={Linkedin} alt="LinkedIn svg"></img>
+          </Link>
         </div>
-
-        {/* 4 sections here */}
-        <div className="footer-sections-container">
-          <div className="footer-section-1">
-            <h4>HEIWA</h4>
-            <p>
-              Lorem ipsum dolor sit amet, eum modus ludus efficiendi ad, in sea
-              ceteros postulant imperdiet, mel ei harum appellantur
-              disputationi.
-            </p>
-            <Buttons size="medium" text="BOOK NOW" />
-          </div>
-
-          <div className="footer-section-2">
-            <h4>IMPORTANT LINKS</h4>
-            <Link to="/about">
-              <p>About Us</p>
-            </Link>
-            <Link to="/about">
-              <p>Book Online</p>
-            </Link>
-            <Link to="/about">
-              <p>Select Stay</p>
-            </Link>
-          </div>
-
-          <div className="footer-section-3">
-            <h4>FIND US AT</h4>
-            <p>Heiwa Wellness, Trivandrum</p>
-            <p>heiwa@wellness.com</p>
-            <p>Phone: +351 258 548</p>
-          </div>
-
-          <div className="footer-section-4">
-            <h4>FIND US AT</h4>
-            <p>Heiwa Wellness, Trivandrum</p>
-            <p>heiwa@wellness.com</p>
-            <p>Phone: +351 258 548</p>
-          </div>
-
+        <a href="mailto:heiwa.wellness@gmail.com">
+          <p className="black company-email">heiwa.wellness@gmail.com</p>
+        </a>
+        <div className="important-links">
+          <Link to="/">
+            <p>Home</p>
+          </Link>
+          <Link to="/about">
+            <p>About</p>
+          </Link>
+          <Link to="/">
+            <p>Our Services</p>
+          </Link>
         </div>
-
-        {/* Copyrights */}
-        <div className="copyrights-container">
-          <hr className="divider" />
-          <p>©Heiwa Tourism & Wellness center, All Rights Reserved</p>
+        <div className="footer-logo">
+          <Link to={"/"}>
+            <img src={footerLogo} alt="footerLogo"></img>
+          </Link>
         </div>
-      </footer>
-    </div>
+        <p className="black copyrights">
+          ©Heiwa Tourism & Wellness center, All Rights Reserved
+        </p>
+      </div>
+    </footer>
   );
 }
 
--- src/components/Footer/Footer1.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Footer1.css";
-
-function Footer1() {
-  return <footer className="footer-1"></footer>;
-}
-
-export default Footer1;
