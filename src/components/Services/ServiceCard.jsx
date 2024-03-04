6bf50c924ed806847247e1c5476a3916ab7f7dbd
Muhammad-Pumex <muhammad@pumexinfotech.com>
2024-03-04 15:22:14 +0530

    Service cards fix.

--- src/components/Services/ServiceCard.jsx
+++ src/components/Services/ServiceCard.jsx
@@ -3,12 +3,14 @@ import "./Services.css";
 
 const ServiceCard = ({ image, title }) => {
   return (
-    <div className="service-card">
-      <span className="service-card-arch"></span>
-      <div className="service-card-image">
-        <img src={image} alt={`${title} service card`}></img>
+    <div className="service-card-wrapper">
+      <div className="service-card">
+        <span className="service-card-arch"></span>
+        <div className="service-card-image">
+          <img src={image} alt={`${title} service card`}></img>
+        </div>
+        <h5 className="black">{title}</h5>
       </div>
-      <h5 className="black">{title}</h5>
     </div>
   );
 };
