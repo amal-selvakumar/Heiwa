e8daac156d94ef5edaff085495e65e1f56ec61f1
Muhammad-Pumex <muhammad@pumexinfotech.com>
2024-03-06 11:04:05 +0530

    Add Contact path

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
 import Home from "./pages/home/Home";
 
 ReactDOM.render(
@@ -9,6 +10,7 @@ ReactDOM.render(
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
       {/* <Route path="/services" element={<Services />} /> */}
       {/* <Route path="/contact" element={<Contact />} /> */}
     </Routes>
