180df799bd572defbedbbb8af1367224ed0c72ec
Muhammad-Pumex <muhammad@pumexinfotech.com>
2024-03-15 20:15:41 +0530

    Remove div for button

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import "./Buttons.css";
 
 const Buttons = ({ size, text }) => {
-  return (
-    <div>
-      <button className={`button-${size} button-common`}>{text}</button>
-    </div>
-  );
+  return <button className={`button-${size} button-common`}>{text}</button>;
 };
 
 export default Buttons;
