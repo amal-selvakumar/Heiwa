4a04e2e23debe6c3aa2c7d0b6e2e46135281af96
Muhammad-Pumex <muhammad@pumexinfotech.com>
2024-03-15 20:16:18 +0530

    Added wrapper for z-index.

--- src/components/WhatsappPopup/WhatsappPopup.jsx
+++ src/components/WhatsappPopup/WhatsappPopup.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import whatsappPopup from "../../assets/whatsapp-popup.png";
-import './WhatsappPopup.css';
+import "./WhatsappPopup.css";
 
 function WhatsappPopup() {
   return (
     <a href="/contact">
       <div className="whatsapp-popup-container">
-        <div className="whatsapp-popup-image">
-          <img src={whatsappPopup} alt="whatsapp-popup"></img>
+        <div className="whatsapp-popup-wrapper">
+          <div className="whatsapp-popup-image">
+            <img src={whatsappPopup} alt="whatsapp-popup"></img>
+          </div>
         </div>
       </div>
     </a>
