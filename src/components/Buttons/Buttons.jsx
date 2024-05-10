7b360192d6fc9226b2756e31ae215125315a0eb7
Muhammad-Pumex <muhammad@pumexinfotech.com>
2024-05-10 16:12:23 +0530

    Deleted all

--- src/components/Buttons/Buttons.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Buttons.css";
-
-const Buttons = ({ size, text }) => {
-  return <button className={`button-${size} button-common`}>{text}</button>;
-};
-
-export default Buttons;
